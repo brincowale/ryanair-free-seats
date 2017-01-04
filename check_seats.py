a9af21e28d57d484ac96bafb1f1e0deda1ea455e
eyalzek <ez@hubrick.com>
2017-01-04 12:11:42 +0100

    Code refactoring
    - change the logic of the program to run the check on a certain interval
    - use `argparse` and set required arguments
    - use class variables instead of passing variable between class functions
    - extract some repeated logic into functions
    - update url, xpath selectors, etc.
    - code formatting; string interpolation, break long statements to multiple lines
    - add some logging and screenshots for easier debugging

--- check_seats.py
+++ check_seats.py
@@ -1,4 +1,6 @@
+import time
 import datetime
+import argparse
 from PIL import Image
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -9,46 +11,102 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class Ryanair:
 
-    def __init__(self, reservation_number, mail, origin, destination):
-        driver = self.create_driver()
-        driver = self.select_paid_seat(driver, reservation_number, mail, origin, destination)
-        self.get_captcha_square(driver)
-        driver.quit()
+    def __init__(self, args):
+        self.driver = self.create_driver()
+        self.reservation = args.reservation
+        self.email = args.email
+        self.check_seat()
+
+    def check_seat(self):
+        self.get_to_seat_map()
+        self.save_seat_map()
+        self.driver.quit()
 
     def create_driver(self):
         # spoof the useragent
         dcap = dict(DesiredCapabilities.PHANTOMJS)
-        dcap["phantomjs.page.settings.userAgent"] = ("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:32.0) Gecko/20100101 Firefox/46.0")
+        dcap["phantomjs.page.settings.userAgent"] = (
+            "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:32.0) Gecko/20100101 Firefox/46.0")
         # set options from browser
-        service_args = ['--load-images=true', '--proxy-type=None', '--ignore-ssl-errors=true', '--webdriver-loglevel=OFF', '--ssl-protocol=tlsv1', '--debug=false']
-        driver = webdriver.PhantomJS(desired_capabilities=dcap, service_args=service_args)
+        service_args = ['--load-images=true', '--proxy-type=None', '--ignore-ssl-errors=true',
+                        '--webdriver-loglevel=OFF', '--ssl-protocol=tlsv1', '--debug=false']
+        driver = webdriver.PhantomJS(
+            desired_capabilities=dcap, service_args=service_args)
         # Set window size
         driver.set_window_size(2400, 2400)
         return driver
 
-    def select_paid_seat(self, driver, reservation_number, mail, origin, destination):
-        driver.get('https://www.bookryanair.com/SkySales/Booking.aspx?culture=es-ES&amp;lc=es-ES#Security')
-        driver.find_element_by_xpath("//input[@id='BookingRetrieveSecurity_CONFIRMATIONNUMBER1']").send_keys(reservation_number)
-        driver.find_element_by_xpath("//input[@id='BookingRetrieveSecurity_CONTACTEMAIL1']").send_keys(mail)
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//select[@id='BookingRetrieveSecurity_ORIGINCITY1']//option[@value='" + origin + "']"))).click()
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//select[@id='BookingRetrieveSecurity_DESTINATIONCITY1']//option[@value='" + destination + "']"))).click()
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@id='BookingRetrieveSecurity_Buttons_Container']/button[@type='submit']"))).click()
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='submitbutton']"))).click()
-        # select paid seat, from 30 days before fly
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='seatOpt1']"))).click()
-        return driver
+    def get_to_seat_map(self):
+        print('getting url')
+        self.driver.get('https://www.ryanair.com/gb/en/')
+        self.screenshot('homepage')
+        self.element_wait(
+            "//*[@id='manage-trips']/a"
+        ).click()
+        self.screenshot('clicked')
+        self.element_wait(
+            "//*[@name='reservationNum']"
+        ).send_keys(self.reservation)
+        self.element_wait(
+            "//*[@name='email']"
+        ).send_keys(self.email)
+        self.screenshot('filled')
+        self.element_wait(
+            "button[translate='trips.my_trips.go']", By.CSS_SELECTOR
+        ).click()
+        self.screenshot('manage_page')
+        self.element_wait(
+            "button[translate='trips.active.spotlight.seats.button']", By.CSS_SELECTOR
+        ).click()
+        self.element_wait(
+            'seat-type-icon', By.CLASS_NAME)
+        self.screenshot('seat_map')
 
-    def get_captcha_square(self, driver):
-        element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='cabin fll']")))
-        location = element.location
-        size = element.size
-        # get the size and position of element
-        left = location['x']
-        top = location['y']
-        right = location['x'] + size['width']
-        bottom = location['y'] + size['height']
-        # save image only from seats
-        driver.save_screenshot('seats_page.png')
+    def element_wait(self, selector, by=By.XPATH, timeout=20):
+        print('Waiting for element %s by %s' % (selector, by))
+        return WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable(
+            (by, selector)))
+
+    def save_seat_map(self):
+        # get dimenstion of seat map
+        element = self.element_wait("//div[@class='seat-map-plane']")
+        left = int(element.location['x'])
+        top = int(element.location['y'])
+        right = left + element.size['width']
+        bottom = top + element.size['height']
+        # take screenshot and crop it
+        self.screenshot('seats_page')
         im = Image.open('seats_page.png')
         im = im.crop((left, top, right, bottom))
-        im.save('seats_' + datetime.datetime.now().strftime("%H-%M-%S-%M%p_%B-%d-%Y") + '.png')
+        filename = 'seats_%s.png' % datetime.datetime.now().strftime("%H-%M-%S-%M%p_%B-%d-%Y")
+        print('Saving %s' % filename)
+        im.save(filename)
+
+    def screenshot(self, filename):
+        print('Saving screenshot as: %s.png' % filename)
+        self.driver.get_screenshot_as_file('%s.png' % filename)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--reservation', required=True,
+                        help='Flight reservation number')
+    parser.add_argument('-e', '--email', required=True,
+                        help='Email address used for reservation')
+    parser.add_argument('-t', '--poll-interval', type=int, default=600,
+                        help='Time in seconds between checks')
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    print(args)
+    while True:
+        print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+        Ryanair(args)
+        print('Sleeping for %d seconds' % args.poll_interval)
+        time.sleep(args.poll_interval)
+
+
+if __name__ == '__main__':
+    main()
